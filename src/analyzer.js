e387a58f33aad3803d3ccb3acf15fa5cb3aab14f
Elisantib <elisa.santibaez1994@gmail.com>
2023-12-20 12:57:51 -0300

    primet commit con funciones de js  y html

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,20 +1,91 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const word = text;
+    const arrayWord = word.split(/\s+/);
+
+    return arrayWord.length;
+
+    //transformar a text en un array con split
+    //solo contabilizar las palabras con una expresion regular
+    //retornar resultado
   },
+
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    const caracter = text;
+    const arrayCharacter = caracter.split("");
+
+    return arrayCharacter.length;
+
+    //Transformar text a un array
+    //contar el largo del array resultante con length
   },
   getCharacterCountExcludingSpaces: (text) => {
+    const onlyCharacter = text;
+    const excluyendoEspacios = onlyCharacter.replace(/\s+/g, "");
+    const arraySpace = excluyendoEspacios.split("");
+
+    return arraySpace.length;
+
+    //---------------------------
+
+    /* return text.replace(/\s+/g, "").length; */
+
+    //----------------------------
+
+    //guardar text en una constante
+    //de esa constante extraer solo los caracteres con una expresion regular
+    //luego transformar a un array
+    //retornar
+
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
+    //
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const number = text;
+    const onlyNumber = number.replace(/[^0-9]+/g, "");
+    const arrayNumber = onlyNumber.split("");
+
+    return arrayNumber.length;
+
+    //guardar text en una constante
+    //solo seleccionar los numeros del texto
+    //convertir los numeros en un array y luego con sumar cuantos numeros habían en mi array
+    //El replace es reemplaza todos los caracteres que no son dígitos (0-9) con una cadena vacía ("").
   },
   getNumberSum: (text) => {
+    const numberSum = text;
+    const arraySumNumber = numberSum.split("");
+    /* console.log(arraySumNumber); */
+    let contador = 0;
+
+    const soloEnteros = [];
+
+    for (let i = 0; i < arraySumNumber.length; i++) {
+      const num = parseInt(arraySumNumber[i]);
+      if (!isNaN(num)) {
+        soloEnteros.push(num);
+      }
+    }
+
+    for (let i = 0; i < soloEnteros.length; i++) {
+      contador = contador + soloEnteros[i];
+    }
+
+    return contador;
+    /* return soloEnteros.reduce((a, b) => a + b, 0); */
+
+    /* console.log(soloEnteros); */
+
+    //--------------------------
+
+    //convertir texto en un array
+    //con for recorrer el array
+    //solo reconocer los numeros
+    //almacenar los numeros en una const
+    //sumar los numeros guardados
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,83 @@
-import analyzer from './analyzer.js';
-
+import analyzer from "./analyzer.js";
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+/* --------------BOTON DE BORRADO--------------- */
+
+document.getElementsByName("btn")[0].addEventListener("click", function () {
+  borrarTexto();
+});
+
+function borrarTexto() {
+  document.getElementsByName("user-input")[0].value = "";
+
+  /* reiniciar palabras */
+  document.getElementById("word").innerHTML = "";
+
+  /* reiniciar caracteres */
+  document.getElementById("characters").innerHTML = "";
+
+  /* reiniciar caracteres sin espacio */
+  document.getElementById("space").innerHTML = "";
+
+  /* reiniciar numeros */
+  document.getElementById("number").innerHTML = "";
+
+  /* reiniciar suma de numeros */
+  document.getElementById("sum").innerHTML = "";
+
+  /* reiniciar longitud */
+  document.getElementById("length").innerHTML = "";
+}
+
+//---------------------------------------------------------------------------------------
+
+/* creo la constante "texto" para guardar el elemento de id "textarea"  */
+
+const texto = document.querySelector("textarea");
+/* console.log(texto); */
+
+/* ------------------ CONTAR PALABRAS --------------------- */
+
+const resultCountWord = document.getElementById("word");
+
+texto.addEventListener("input", function () {
+  resultCountWord.textContent = analyzer.getWordCount(texto.value);
+});
+
+/* ------------------ CONTAR CARACTERES --------------------- */
+
+/* creo la constante "resultCountCharacters" para guardar el elemento de id "characters" */
+
+const resultCountCharacters = document.getElementById("characters");
+
+/* En el siguiente codigo le decimos a la const texto que escuche el evento input que se dispara cuando el contenido del elemento cambia y luego de eso ejecute la función */
+/* En la función decimos que el resultado del contenido de la const resultCountCharacters debe ser el valor de la const texto que se encuentra en getCharacterCount  */
+
+texto.addEventListener("input", function () {
+  resultCountCharacters.textContent = analyzer.getCharacterCount(texto.value);
+});
+
+/*----------------------- CONTAR CARACTERES SIN ESPACIOS -----------------*/
+
+const resultCharacterCountExcludingSpaces = document.getElementById("space");
+
+texto.addEventListener("input", function () {
+  resultCharacterCountExcludingSpaces.textContent =
+    analyzer.getCharacterCountExcludingSpaces(texto.value);
+});
+
+/* -------------------- CONTAR NUMEROS ----------------------------- */
+
+const resultNumerCount = document.getElementById("number");
+
+texto.addEventListener("input", function () {
+  resultNumerCount.textContent = analyzer.getNumberCount(texto.value);
+});
+
+/* -------------------- SUMAR NUMEROS ----------------------------- */
+
+const resultNumberSum = document.getElementById("sum");
+
+texto.addEventListener("input", function () {
+  resultNumberSum.textContent = analyzer.getNumberSum(texto.value);
+});
