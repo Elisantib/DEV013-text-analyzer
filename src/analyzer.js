3d87b931e3de5224180f290014e79a152ea38689
Elisantib <elisa.santibaez1994@gmail.com>
2024-01-03 21:36:07 -0300

    modificaciones en funciones de analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -21,9 +21,10 @@ const analyzer = {
   },
   getCharacterCountExcludingSpaces: (text) => {
     const onlyCharacter = text;
-    const excluyendoEspacios = onlyCharacter.replace(/\s+/g, "");
+    const excluyendoEspacios = onlyCharacter
+      .replace(/[^\w\s]|_/g, "")
+      .replace(/\s+/g, "");
     const arraySpace = excluyendoEspacios.split("");
-    /* console.log(arraySpace); */
 
     return arraySpace.length;
 
@@ -42,10 +43,16 @@ const analyzer = {
   },
   getAverageWordLength: (text) => {
     const wordLength = text;
+    const words = wordLength.split(/\s+/);
+    const totalWordLength = words.reduce((acc, word) => acc + word.length, 0);
+    const averageWordLength = totalWordLength / words.length;
+    return averageWordLength;
+
+    /* const wordLength = text;
     const onlyWord2 = wordLength.split(/\s+/).length;
     const onlyCharacter2 = wordLength.split("").length;
 
-    return onlyCharacter2 / onlyWord2;
+    return onlyCharacter2 / onlyWord2; */
 
     //Guardar text en una constante para utilizarla
     //seleccionar solo las palabras del texto
@@ -55,10 +62,20 @@ const analyzer = {
   },
   getNumberCount: (text) => {
     const number = text;
+    const numbersArray = number.split(" ");
+
+    const onlyNumber = numbersArray
+      .filter((valor) => /^\d+$/.test(String(valor)))
+      .map(Number);
+
+    console.log(onlyNumber);
+    return onlyNumber.length;
+
+    /* const number = text;
     const onlyNumber = number.replace(/[^0-9]+/g, "");
     const arrayNumber = onlyNumber.split("");
 
-    return arrayNumber.length;
+    return arrayNumber.length; */
 
     //guardar text en una constante
     //solo seleccionar los numeros del texto
@@ -67,8 +84,43 @@ const analyzer = {
   },
   getNumberSum: (text) => {
     const numberSum = text;
+    const numbersSumArray = numberSum.match(/\b\d+(\.\d+)?\b/g);
+
+    if (!numbersSumArray) {
+      return 0;
+    }
+
+    const onlyNumberSum = numbersSumArray.map(Number);
+
+    const suma = onlyNumberSum.reduce(
+      (acumulador, numero) => acumulador + numero,
+      0
+    );
+
+    return suma;
+
+    //---------------------------------------------
+
+    /* const numberSum = text;
+    const numbersSumArray = numberSum.split(" ");
+
+    const onlyNumberSum = numbersSumArray
+      .filter((valor) => /^\d+$/.test(String(valor)))
+      .map(Number);
+
+    const suma = onlyNumberSum.reduce(
+      (acumulador, numero) => acumulador + numero,
+      0
+    );
+
+    console.log(suma);
+    return suma; */
+
+    //----------------------------------------------
+
+    /* const numberSum = text;
     const arraySumNumber = numberSum.split("");
-    /* console.log(arraySumNumber); */
+
     let contador = 0;
 
     const soloEnteros = [];
@@ -84,10 +136,7 @@ const analyzer = {
       contador = contador + soloEnteros[i];
     }
 
-    return contador;
-    /* return soloEnteros.reduce((a, b) => a + b, 0); */
-
-    /* console.log(soloEnteros); */
+    return contador; */
 
     //--------------------------
 
